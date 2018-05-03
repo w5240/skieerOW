1385babc598f8b7bf9c71aa44f2352308bf6e004
Eddy.Wang <1027220189@qq.com>
2018-05-03 12:23:23 +0800

    project 页编写

--- src/pages/home/components/project.js
+++ src/pages/home/components/project.js
@@ -3,7 +3,14 @@ import React , {Component} from 'react';
 export default class Project extends Component{
   render(){
     return(
-      <div>proj</div>
+      <div className="project">
+        <div className="banner">
+          <p>解决方案</p>
+          <div className="content">
+            依托八爪鱼、微图、数多多等大数据及人工智能产品，为各行业打造大数据场景整体解决方案，使企业能高效、便捷地进行数据运用和价值实现。
+          </div>
+        </div>
+      </div>
     )
   }
 }
